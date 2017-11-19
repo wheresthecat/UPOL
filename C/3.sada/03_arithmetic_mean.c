be56d7c9f93b036d5f3da2f96cac9c5f67285254
Honza Kováč <frosty@centrum.cz>
2017-11-19 02:20:16 +0100

    Pridano hotove cviko 03-03
    Je treba jeste zkontrolovat a pote hodit do masteru.

--- C/3.sada/03_arithmetic_mean.c
+++ C/3.sada/03_arithmetic_mean.c
@@ -10,7 +10,7 @@ Program vypocita aritmeticky prumer pole z prvku zadanych uzivatelem.
 
 /***
 TODO:
-    -automaticke naplneni pole pomoci rand() % size
+    Konecna kontrola a okomentovani.
 */
 
 int main()
@@ -19,29 +19,91 @@ int main()
     // Deklarace promennych
     int size = 0;
     int sum = 0;
+    int repeat = 1;
+    int autoFill = 0;
+    int randmax = 0;
+
+    unsigned char choice;
     float meanValue;
+
 
 
 
     // Uzivatelsky vstup: velikost pole.
     printf("Zadejte pocet prvku pole: \n");
     scanf("%i", &size);
-    printf("\n");
-    if(size > 20)
+
+    printf("\nChcete zadat cisla (M)anualne, nebo (A)utomaticky?\n");
+
+    while(repeat == 1)
     {
-        printf("\nNo tak to teda good luck.\n");
+
+
+        scanf(" %c", &choice);
+        getchar();
+
+        switch(choice)
+        {
+            case 'A':
+            case 'a':
+                autoFill = 1;
+                repeat = 0;
+
+            break;
+
+            case 'M':
+            case 'm':
+                printf("Manualne.\n");
+                repeat = 0;
+            break;
+
+
+            default:
+                printf("Chyba zadani. Zvolte prosim A nebo M:\n");
+                repeat = 1;
+                break;
+
+
+        }
+
+
     }
+    printf("\n");
+
 
     // Deklarace pole.
     int numbers[size]; // Kdyz byla deklarace pole jeste pred vstupem, hazelo to vcelku zvlastni bugy.
 
     // Uzivatelsky vstup: prvky pole.
 
-    for(int i = 0; i < size; i++)
+
+    if(autoFill == 1)
     {
-        printf("Zadejte %i. cislo: ", i + 1);
-        scanf("%i", &numbers[i]);
+        printf("Zadejte maximalni velikost nahodneho cisla: \n");
+        scanf(" %i", &randmax);
+
+        for(int i = 0; i < size; i++)
+        {
+            printf("\nZadano %i. cislo: %i", i + 1, numbers[i] = rand() % randmax);
+            //scanf("%i", &numbers[i]);
+        }
+        printf("\n");
+
     }
+    else
+    {
+        if(size > 20)
+        {
+            printf("\nGood Luck.\n");
+        }
+        for(int i = 0; i < size; i++)
+        {
+            printf("Zadejte %i. cislo: ", i + 1);
+            scanf("%i", &numbers[i]);
+        }
+    }
+
+
 
 
     printf("======================================\n");
@@ -80,4 +142,6 @@ int main()
 
     getchar();
     return 0;
+
+
 }
