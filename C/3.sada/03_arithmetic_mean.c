fd0e66a98519e7dfe4ce9795eb1885119da3405b
Honza Kováč <frosty@centrum.cz>
2017-11-19 20:49:29 +0100

    Hotove cviko 03-03

--- C/3.sada/03_arithmetic_mean.c
+++ C/3.sada/03_arithmetic_mean.c
@@ -8,44 +8,48 @@ Program vypocita aritmeticky prumer pole z prvku zadanych uzivatelem.
 #include <stdio.h>
 #include <stdlib.h>
 
-/***
-TODO:
-    Konecna kontrola a okomentovani.
-*/
+
 
 int main()
 {
 
     // Deklarace promennych
-    int size = 0;
-    int sum = 0;
-    int repeat = 1;
-    int autoFill = 0;
-    int randmax = 0;
+    int size = 0; // Velikost pole.
+    int sum = 0; // Suma pole.
+    int repeat = 1; // Promenna pro cyklus v pripade chybneho vstupu.
+    int autoFill = 0; // "bool" pro manualni / automaticke plneni.
+    int randmax = 0; // Promena pro strop pro funkci rand().
 
-    unsigned char choice;
-    float meanValue;
+    char choice; // Pro switch vyberu zpusobu zadani cisel.
+    float meanValue; // Konecny vysledek, prumer hodnot v poli.
+
 
 
 
 
     // Uzivatelsky vstup: velikost pole.
     printf("Zadejte pocet prvku pole: \n");
-    scanf("%i", &size);
+    scanf(" %i", &size);
 
     printf("\nChcete zadat cisla (M)anualne, nebo (A)utomaticky?\n");
 
     while(repeat == 1)
     {
-
-
+        /* Zde jsem mel potize se switchem. Program ignoroval vlozene hodnoty a bral to jako chybny
+        * vstup, i když mel byt spravny a obcas to vypadalo, že to nejak ignoruje break prikaz. Po nejake dobe
+        * jsem to castecne vyresil vlozenim getchar() hned po scanf(), ale posleze se ukazalo, ze to vubec nepomohlo.
+        * Nakonec stacilo pridat mezeru pred %c ve scanf() - díky mezeře bude scanf ignorovat bile znaky po stisku enteru apod.
+        * Coz jsem nasel na stackoverflow.com.
+        * ( https://stackoverflow.com/questions/29122166/c-scanf-for-char-not-working-as-expected?rq=1 )
+        */
         scanf(" %c", &choice);
         getchar();
-
+        // Switch pro zvoleni metody naplneni pole cisli.
         switch(choice)
         {
             case 'A':
             case 'a':
+                printf("Automaticky.\n");
                 autoFill = 1;
                 repeat = 0;
 
@@ -55,6 +59,7 @@ int main()
             case 'm':
                 printf("Manualne.\n");
                 repeat = 0;
+
             break;
 
 
@@ -63,21 +68,18 @@ int main()
                 repeat = 1;
                 break;
 
-
         }
-
-
     }
     printf("\n");
 
 
     // Deklarace pole.
-    int numbers[size]; // Kdyz byla deklarace pole jeste pred vstupem, hazelo to vcelku zvlastni bugy.
+    int numbers[size]; // Kdyz byla deklarace pole jeste pred vstupem (tzn int size mel hodnotu 0), hazelo to vcelku zvlastni bugy.
 
     // Uzivatelsky vstup: prvky pole.
 
 
-    if(autoFill == 1)
+    if(autoFill == 1) // Automaticke naplneni pole.
     {
         printf("Zadejte maximalni velikost nahodneho cisla: \n");
         scanf(" %i", &randmax);
@@ -85,16 +87,16 @@ int main()
         for(int i = 0; i < size; i++)
         {
             printf("\nZadano %i. cislo: %i", i + 1, numbers[i] = rand() % randmax);
-            //scanf("%i", &numbers[i]);
         }
         printf("\n");
-
     }
-    else
+
+
+    else // Manualni naplneni pole.
     {
         if(size > 20)
         {
-            printf("\nGood Luck.\n");
+            printf("\nGood Luck.\n"); // Motivace pro uzivatele.
         }
         for(int i = 0; i < size; i++)
         {
@@ -104,9 +106,7 @@ int main()
     }
 
 
-
-
-    printf("======================================\n");
+    printf("\n======================================\n");
     // Vypis prvku pole:
     printf("Pole obsahuje nasledujici prvky: ");
     for(int j = 0; j < size; j++)
@@ -130,15 +130,16 @@ int main()
         sum += numbers[j];
     }
 
-    // printf("\nSuma pole: %i", sum);
 
     // Vypocet prumeru.
 
-    meanValue = sum / size;
+    meanValue = (float)sum / size;
 
     // Vystup:
 
     printf("\nAritmeticky prumer pole je: %.2f", meanValue);
+
+
 
     getchar();
     return 0;
