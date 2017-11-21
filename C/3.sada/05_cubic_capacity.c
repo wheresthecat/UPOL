9c03c965280f525f4a79d50e270d8e634896477c
Honza Kováč <frosty@centrum.cz>
2017-11-21 16:02:40 +0100

    03-05

--- C/3.sada/05_cubic_capacity.c
+++ C/3.sada/05_cubic_capacity.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PI 3.14159265359
+
 
 // TODO Deklarace funkce.
 // Povrch objem kvadr
 // Povrch objem valec
 // Plus jeste neco.
+
+/**
+Obsah kruhu - piR^2
+Obvod kruhu - 2piR nebo piD
+
+objem kvadru - a*b*c
+povrch kvadru -
+*/
 
 
 int main()
