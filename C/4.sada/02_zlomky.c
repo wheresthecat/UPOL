ca09fd287353b39362bbe91e6d586e0b3cd04e58
Honza Kováč <frosty@centrum.cz>
2018-03-25 20:17:26 +0200

    Druhe cviko

---
+++ C/4.sada/02_zlomky.c
@@ -0,0 +1,41 @@
+// Cviko z upolu, secitani zlomku, strukturovane datove typy a funkce.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG 1
+
+
+typedef struct {
+	int citatel;
+	int jmenovatel;
+} zlomek;
+
+zlomek vysledek(zlomek a, zlomek b);
+
+int main(int argc, char const *argv[])
+{
+
+	zlomek a, b;
+
+	printf("Scitani zlomku: \n");
+
+	printf("Vlozte zlomek a: (citatel/jmenovatel) \n");
+	scanf("%d/%d", &a.citatel, &a.jmenovatel);
+
+	printf("Vlozte zlomek b: (citatel/jmenovatel) \n");
+	scanf("%d/%d", &b.citatel, &b.jmenovatel);
+
+
+
+	#ifdef DEBUG
+
+	printf("Zlomek a: %d/%d.\n", a.citatel, a.jmenovatel);
+	printf("Zlomek a: %d/%d.\n", b.citatel, b.jmenovatel);
+
+	#endif
+
+
+
+	return 0;
+}
